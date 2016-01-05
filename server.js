93a11dc2ae53054a7c0065b5855ea91f97d63577
Pete Cook <pete@cookpete.com>
2016-01-05 11:55:26 +0000

    Rebase on react-transform-boilerplate
    Uses https://github.com/CookPete/react-transform-boilerplate
    Closes https://github.com/CookPete/react-player/issues/8

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
+var path = require('path')
+var express = require('express')
 var webpack = require('webpack')
-var WebpackDevServer = require('webpack-dev-server')
-var config = require('./webpack.config')
+var config = require('./webpack.config.dev')
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+var app = express()
+var compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(3000, 'localhost', function (err) {
   if (err) {
     console.log(err)
+    return
   }
 
-  console.log('Listening at localhost:3000')
+  console.log('Listening at http://localhost:3000')
 })
