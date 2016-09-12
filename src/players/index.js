2958cd2365828ae4785f23193fe80dd811649f14
Pete Cook <pete@cookpete.com>
2016-09-12 08:22:28 +0100

    Remove unused player index file

--- src/players/index.js
+++
@@ -1,6 +0,0 @@
-import YouTube from './YouTube'
-import SoundCloud from './SoundCloud'
-import Vimeo from './Vimeo'
-import FilePlayer from './FilePlayer'
-
-export default [ YouTube, SoundCloud, Vimeo, FilePlayer ]
