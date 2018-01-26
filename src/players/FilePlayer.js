54962d0dd701d16ded5cf5414634230a9e7851f0
Pete Cook <pete@cookpete.com>
2018-01-26 21:30:42 +0000

    Guard against navigator not being available
    Fixes https://github.com/CookPete/react-player/issues/315

--- src/players/FilePlayer.js
+++ src/players/FilePlayer.js
@@ -80,7 +80,10 @@ export default class FilePlayer extends Component {
     return AUDIO_EXTENSIONS.test(props.url) || props.config.file.forceAudio
   }
   shouldUseHLS (url) {
-    const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+    const iOS =
+      typeof navigator !== 'undefined' &&
+      /iPad|iPhone|iPod/.test(navigator.userAgent) &&
+      !window.MSStream
     return (HLS_EXTENSIONS.test(url) && !iOS) || this.props.config.file.forceHLS
   }
   shouldUseDASH (url) {
