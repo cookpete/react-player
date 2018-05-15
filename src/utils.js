5604414989f53ee989751d3ce53e8ae79a7be24d
Pete Cook <pete@cookpete.com>
2018-05-15 15:03:46 +0100

    Prevent loading SDKs multiple times
    Fixes https://github.com/CookPete/react-player/issues/391

--- src/utils.js
+++ src/utils.js
@@ -50,22 +50,34 @@ export function queryString (object) {
 
 // Util function to load an external SDK
 // or return the SDK if it is already loaded
+const resolves = {}
 export function getSDK (url, sdkGlobal, sdkReady = null, isLoaded = () => true) {
   if (window[sdkGlobal] && isLoaded(window[sdkGlobal])) {
     return Promise.resolve(window[sdkGlobal])
   }
   return new Promise((resolve, reject) => {
+    // If we are already loading the SDK, add the resolve
+    // function to the existing array of resolve functions
+    if (resolves[url]) {
+      resolves[url].push(resolve)
+      return
+    }
+    resolves[url] = [resolve]
+    const onLoaded = sdk => {
+      // When loaded, resolve all pending promises
+      resolves[url].forEach(resolve => resolve(sdk))
+    }
     if (sdkReady) {
       const previousOnReady = window[sdkReady]
       window[sdkReady] = function () {
         if (previousOnReady) previousOnReady()
-        resolve(window[sdkGlobal])
+        onLoaded(window[sdkGlobal])
       }
     }
     loadScript(url, err => {
       if (err) reject(err)
       if (!sdkReady) {
-        resolve(window[sdkGlobal])
+        onLoaded(window[sdkGlobal])
       }
     })
   })
