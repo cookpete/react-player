0f7b684396cef8c43807778f7b855ead9323016f
Pete Cook <pete@cookpete.com>
2018-05-15 08:41:54 +0100

    Clear duration check timeout when unmounting

--- src/Player.js
+++ src/Player.js
@@ -22,6 +22,7 @@ export default class Player extends Component {
   }
   componentWillUnmount () {
     clearTimeout(this.progressTimeout)
+    clearTimeout(this.durationCheckTimeout)
     if (this.isReady) {
       this.player.stop()
     }
