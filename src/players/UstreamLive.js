a2fa6aab40b2f29f007b37a9d82ddbabd02a374a
Steven Anderson <steven@sjanderson.org>
2018-12-10 16:13:55 -0800

    fix ustream 2 (#21)
    * test trial
    
    Signed-off-by: Steven Anderson <steven@sjanderson.org>
    
    * fix autoplay for ustream!
    
    Signed-off-by: Steven Anderson <steven@sjanderson.org>

--- src/players/UstreamLive.js
+++ src/players/UstreamLive.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { callPlayer, getSDK, randomString, queryString } from '../utils'
+import { callPlayer, getSDK, randomString } from '../utils'
 import createSinglePlayer from '../singlePlayer'
 
 const SDK_URL = 'http://developers.ustream.tv/js/ustream-embedapi.min.js'
@@ -9,7 +9,7 @@ const MATCH_URL = /(ustream.tv\/channel\/)([^#&?/]*)/
 const PLAYER_ID_PREFIX = 'UstreamLive-player-'
 export class UstreamLive extends Component {
   static displayName = 'UstreamLive';
-  static canPlay = url => MATCH_URL.test(url);
+  static canPlay = url => MATCH_URL.test(url)
   static loopOnEnded = false;
 
   playerID = PLAYER_ID_PREFIX + randomString()
@@ -19,32 +19,41 @@ export class UstreamLive extends Component {
     const m = url.match(MATCH_URL)
     return m[2]
   }
-  load (url) {
+  componentDidUpdate (prevProps) {
+    // reset autoplay on reload
+    if (prevProps.url !== this.props.url) {
+      this.autoplay = null
+    }
+  }
+
+  load () {
+    const {onEnded, onError, onPause, onPlay, onReady, playing} = this.props
     getSDK(SDK_URL, SDK_GLOBAL).then(UstreamEmbed => {
       if (!this.container) return
+      this.autoplay = playing
+      this.currentTime = 0
       this.player = UstreamEmbed(this.playerID)
-      this.player.currentTime = 0
       this.player.addListener('playing', (type, playing) => {
         if (playing) {
           this.playTime = Date.now()
-          this.props.onPlay()
+          onPlay()
         } else {
-          this.player.currentTime = this.getCurrentTime()
+          this.currentTime = this.getCurrentTime()
           this.playTime = null
-          this.props.onPause()
+          onPause()
         }
       })
       this.player.addListener('live', () => {
-        this.props.onReady()
+        onReady()
       })
       this.player.addListener('offline', () => {
-        this.props.onReady()
+        onReady()
       })
-      this.player.addListener('finished', this.props.onEnded)
+      this.player.addListener('finished', onEnded)
       this.player.getProperty('duration', (duration) => {
         this.player.duration = duration || Infinity
       })
-    }, this.props.onError)
+    }, onError)
   }
   // todo
   mute = () => {}
@@ -73,8 +82,7 @@ export class UstreamLive extends Component {
     if (this.playTime) {
       playing = (Date.now() - this.playTime) / 1000
     }
-
-    return this.player.currentTime + playing
+    return this.currentTime + playing
   }
   getSecondsLoaded () {
     return null
@@ -89,15 +97,16 @@ export class UstreamLive extends Component {
       width: '100%',
       height: '100%'
     }
-    const query = queryString({
-      autoplay: this.props.playing
-    })
+
+    if (![true, false].includes(this.autoplay)) {
+      this.autoplay = this.props.playing
+    }
 
     return (
       <iframe
         id={this.playerID}
         ref={this.ref}
-        src={`${base}/${channelId}?html5ui?${query}`}
+        src={`${base}/${channelId}?html5ui?autoplay=${this.autoplay}`}
         frameBorder='0'
         scrolling='no'
         style={style}
