e37f32d85c505a16791e5ad33be31fb9a9bfad8a
Cody Lundquist <cody.lundquist@gmail.com>
2019-06-05 12:20:56 -0700

    Fix an issue with setting state in the Preview component after being unmounted. (#658)

--- src/Preview.js
+++ src/Preview.js
@@ -3,16 +3,21 @@ import React, { Component } from 'react'
 const ICON_SIZE = '64px'
 
 export default class Preview extends Component {
+  mounted = false
   state = {
     image: null
   }
   componentDidMount () {
+    this.mounted = true
     this.fetchImage(this.props)
   }
   componentWillReceiveProps (nextProps) {
     if (this.props.url !== nextProps.url) {
       this.fetchImage(nextProps)
     }
+  }
+  componentWillUnmount () {
+    this.mounted = false
   }
   fetchImage ({ url, light }) {
     if (typeof light === 'string') {
@@ -23,7 +28,7 @@ export default class Preview extends Component {
     return window.fetch(`https://noembed.com/embed?url=${url}`)
       .then(response => response.json())
       .then(data => {
-        if (data.thumbnail_url) {
+        if (data.thumbnail_url && this.mounted) {
           const image = data.thumbnail_url.replace('height=100', 'height=480')
           this.setState({ image })
         }
