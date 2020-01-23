708e6aaba0df1d7ad0775e81404731a010940d8a
Pete Cook <pete@cookpete.com>
2020-01-23 21:42:40 +0000

    Relax facebook regex
    Not sure there’s any downside to attempting to play any Facebook URL with the FBSDK
    Fixes https://github.com/CookPete/react-player/issues/778

--- src/players/Facebook.js
+++ src/players/Facebook.js
@@ -6,7 +6,7 @@ import createSinglePlayer from '../singlePlayer'
 const SDK_URL = 'https://connect.facebook.net/en_US/sdk.js'
 const SDK_GLOBAL = 'FB'
 const SDK_GLOBAL_READY = 'fbAsyncInit'
-const MATCH_URL = /facebook\.com\/([^/?].+\/)?video(s|\.php)[/?].*$/
+const MATCH_URL = /facebook\.com\/.+/
 const PLAYER_ID_PREFIX = 'facebook-player-'
 
 export class Facebook extends Component {
