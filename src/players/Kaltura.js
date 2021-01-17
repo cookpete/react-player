754b7f718fd8439c7b9a472b03ee48cab792b185
Jess Portnoy <jess.portnoy@kaltura.com>
2021-01-17 16:06:17 +0000

    seeking and vol control work

--- src/players/Kaltura.js
+++ src/players/Kaltura.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 import { callPlayer, getSDK } from '../utils'
 import { canPlay } from '../patterns'
 
-// const SDK_URL = 'https://demo.kaltura.me/player-0.0.11.js'
 const SDK_URL = 'https://cdn.embed.ly/player-0.1.0.min.js'
 const SDK_GLOBAL = 'playerjs'
 
@@ -23,21 +22,14 @@ export default class Kaltura extends Component {
     getSDK(SDK_URL, SDK_GLOBAL).then(playerjs => {
       if (!this.iframe) return
       this.player = new playerjs.Player(this.iframe)
+      this.player.isReady = true
       this.player.setLoop(this.props.loop)
       this.player.on('ready', this.props.onReady)
-      this.player.on('ready', function () {
-        this.isReady = true
-      })
-
       this.player.on('play', this.props.onPlay)
       this.player.on('pause', this.props.onPause)
       this.player.on('seeked', this.props.onSeek)
       this.player.on('ended', this.props.onEnded)
       this.player.on('error', this.props.onError)
-      this.player.on('timeupdate', ({ duration, seconds }) => {
-        this.duration = duration
-        this.currentTime = seconds
-      })
       this.player.on('buffered', ({ percent }) => {
         if (this.duration) {
           this.secondsLoaded = this.duration * percent
@@ -51,6 +43,10 @@ export default class Kaltura extends Component {
 
   play () {
     this.callPlayer('play')
+    this.player.on('timeupdate', ({ duration, seconds }) => {
+      this.duration = duration
+      this.currentTime = seconds
+    })
   }
 
   pause () {
@@ -66,7 +62,7 @@ export default class Kaltura extends Component {
   }
 
   setVolume (fraction) {
-    this.callPlayer('setVolume', fraction * 100)
+    this.callPlayer('setVolume', fraction)
   }
 
   setLoop (loop) {
