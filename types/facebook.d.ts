a3134f9ae2b710e74c7575e6c50a3e15709c3e25
Sam Carton <samcarton@users.noreply.github.com>
2021-10-26 22:31:44 +0800

    Make facebook config type properties optional (#1314)

--- types/facebook.d.ts
+++ types/facebook.d.ts
@@ -1,9 +1,9 @@
 import BaseReactPlayer, { BaseReactPlayerProps } from './base'
 
 export interface FacebookConfig {
-  appId: string
-  version: string
-  playerId: string
+  appId?: string
+  version?: string
+  playerId?: string
 }
 
 export interface FacebookPlayerProps extends BaseReactPlayerProps {
