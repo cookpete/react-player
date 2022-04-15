97c563d67d49418f2c61f03c5fdd97337f90741c
pburinda <paul.burinda@gmail.com>
2022-04-14 22:43:07 -0400

    adds support for youtube short urls

--- test/ReactPlayer/staticMethods.js
+++ test/ReactPlayer/staticMethods.js
@@ -11,6 +11,7 @@ configure({ adapter: new Adapter() })
 
 test('canPlay()', t => {
   t.true(ReactPlayer.canPlay('https://www.youtube.com/watch?v=oUFJJNQGwhk'))
+  t.true(ReactPlayer.canPlay('https://youtube.com/shorts/370kwJ-x5TY?feature=share'))
   t.true(ReactPlayer.canPlay('https://soundcloud.com/miami-nights-1984/accelerated'))
   t.true(ReactPlayer.canPlay('https://www.facebook.com/facebook/videos/10153231379946729'))
   t.true(ReactPlayer.canPlay('https://vimeo.com/90509568'))
