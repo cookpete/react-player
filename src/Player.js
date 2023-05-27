b2876af2b0128587365888629841b8aeff3b599c
David Bazílio Ribeiro <david.bazilio@learninghubz.com>
2023-05-27 11:00:21 +0100

    Keep formatting

--- src/Player.js
+++ src/Player.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import isEqual from 'react-fast-compare';
+import React, { Component } from 'react'
+import isEqual from 'react-fast-compare'
 
-import { defaultProps, propTypes } from './props';
-import { isMediaStream } from './utils';
+import { propTypes, defaultProps } from './props'
+import { isMediaStream } from './utils'
 
 const SEEK_ON_PLAY_EXPIRY = 5000
 
