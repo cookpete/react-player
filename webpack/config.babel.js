b3491be96efa1d8c00e40d595e5765b9ce5908e2
Jesse Portnoy <jesse@packman.io>
2023-08-30 16:48:11 +0100

    Same override is needed here..

--- webpack/config.babel.js
+++ webpack/config.babel.js
@@ -99,3 +99,6 @@ export default {
     hints: false
   }
 }
+const crypto = require('crypto')
+const cryptoOrigCreateHash = crypto.createHash
+crypto.createHash = algorithm => cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm)
