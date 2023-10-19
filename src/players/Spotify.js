b5906c88abb6c8ef503a4407b8a2c0754fcedf31
jared-paul <jaredpaul9820@gmail.com>
2023-10-19 11:20:34 -0700

    adding error handling for iframeapi not ready yet

--- src/players/Spotify.js
+++ src/players/Spotify.js
@@ -1,137 +1,145 @@
-import React, { Component } from 'react'
-import { getSDK, callPlayer } from '../utils'
-import { canPlay } from '../patterns'
+import React, { Component } from "react";
+import { getSDK, callPlayer } from "../utils";
+import { canPlay } from "../patterns";
 
-const SDK_URL = 'https://open.spotify.com/embed-podcast/iframe-api/v1'
-const SDK_GLOBAL = 'SpotifyIframeApi'
-const SDK_GLOBAL_READY = 'SpotifyIframeApi'
+const SDK_URL = "https://open.spotify.com/embed-podcast/iframe-api/v1";
+const SDK_GLOBAL = "SpotifyIframeApi";
+const SDK_GLOBAL_READY = "SpotifyIframeApi";
 
 export default class Spotify extends Component {
-  static displayName = 'Spotify'
-  static loopOnEnded = true
-  static canPlay = canPlay.spotify
-  callPlayer = callPlayer
-  duration = null
-  currentTime = null
-  totalTime = null
-  player = null
+  static displayName = "Spotify";
+  static loopOnEnded = true;
+  static canPlay = canPlay.spotify;
+  callPlayer = callPlayer;
+  duration = null;
+  currentTime = null;
+  totalTime = null;
+  player = null;
 
-  componentDidMount () {
-    this.props.onMount && this.props.onMount(this)
+  componentDidMount() {
+    this.props.onMount && this.props.onMount(this);
   }
 
-  load (url) {
+  load(url) {
     if (window[SDK_GLOBAL] && !this.player) {
-      this.initializePlayer(window[SDK_GLOBAL], url)
-      return
+      this.initializePlayer(window[SDK_GLOBAL], url);
+      return;
     } else if (this.player) {
-      this.callPlayer('loadUri', this.props.url)
-      return
+      this.callPlayer("loadUri", this.props.url);
+      return;
     }
 
-    window.onSpotifyIframeApiReady = (IFrameAPI) => this.initializePlayer(IFrameAPI, url)
-    getSDK(SDK_URL, SDK_GLOBAL, SDK_GLOBAL_READY)
+    window.onSpotifyIframeApiReady = (IFrameAPI) =>
+      this.initializePlayer(IFrameAPI, url);
+    getSDK(SDK_URL, SDK_GLOBAL, SDK_GLOBAL_READY);
   }
 
   initializePlayer = (IFrameAPI, url) => {
-    if (!this.container) return
+    if (!this.container) return;
 
     const options = {
-      width: '100%',
-      height: '100%',
-      uri: url
-    }
+      width: "100%",
+      height: "100%",
+      uri: url,
+    };
     const callback = (EmbedController) => {
-      this.player = EmbedController
-      this.player.addListener('playback_update', this.onStateChange)
-      this.player.addListener('ready', this.props.onReady)
+      this.player = EmbedController;
+      this.player.addListener("playback_update", this.onStateChange);
+      this.player.addListener("ready", this.props.onReady);
+      this.player.loadUri();
+    };
+    if (IFrameAPI && typeof IFrameAPI.createController === "function") {
+      IFrameAPI.createController(this.container, options, callback);
+    } else if (!IFrameAPI) {
+      console.error("yea so IFrameAPI is not available");
+    } else if (typeof IFrameAPI.createController === "function") {
+      console.error("yea so IFrameAPI.createController is not a function");
     }
-    IFrameAPI.createController(this.container, options, callback)
-  }
+  };
 
   onStateChange = (event) => {
-    const { data } = event
-    const { onPlay, onPause, onBuffer, onBufferEnd, onEnded } = this.props
+    const { data } = event;
+    const { onPlay, onPause, onBuffer, onBufferEnd, onEnded } = this.props;
 
     if (data.position >= data.duration && data.position && data.duration) {
-      onEnded()
+      onEnded();
     }
-    if (data.isPaused === true) onPause()
+    if (data.isPaused === true) onPause();
     if (data.isPaused === false && data.isBuffering === false) {
-      this.currentTime = data.position
-      this.totalTime = data.duration
-      onPlay()
-      onBufferEnd()
+      this.currentTime = data.position;
+      this.totalTime = data.duration;
+      onPlay();
+      onBufferEnd();
     }
-    if (data.isBuffering === true) onBuffer()
+    if (data.isBuffering === true) onBuffer();
+  };
+
+  play() {
+    this.callPlayer("resume");
   }
 
-  play () {
-    this.callPlayer('resume')
+  pause() {
+    this.callPlayer("pause");
   }
 
-  pause () {
-    this.callPlayer('pause')
+  stop() {
+    this.callPlayer("destroy");
   }
 
-  stop () {
-    this.callPlayer('destroy')
-  }
-
-  seekTo (amount) {
+  seekTo(amount) {
     this.callPlayer('seek', amount)
     if (!this.props.playing) {
-      this.pause()
+      this.pause();
     } else {
-      this.play()
+      this.play();
     }
   }
 
-  setVolume (fraction) {
+  setVolume(fraction) {
     // No volume support
   }
 
-  mute () {
+  mute() {
     // No volume support
   }
 
-  unmute () {
+  unmute() {
     // No volume support
   }
 
-  setPlaybackRate (rate) {
+  setPlaybackRate(rate) {
     // No playback rate support
   }
 
-  setLoop (loop) {
+  setLoop(loop) {
     // No loop support
   }
 
-  getDuration () {
-    return this.totalTime / 1000
+  getDuration() {
+    return this.totalTime / 1000;
   }
 
-  getCurrentTime () {
-    return this.currentTime / 1000
+  getCurrentTime() {
+    return this.currentTime / 1000;
   }
 
-  getSecondsLoaded () {
+  getSecondsLoaded() {
     // No seconds loaded support
   }
 
-  ref = container => {
-    this.container = container
-  }
+  ref = (container) => {
+    this.container = container;
+  };
 
-  render () {
+  render() {
     const style = {
-      width: '100%',
-      height: '100%'
-    }
+      width: "100%",
+      height: "100%",
+    };
     return (
       <div style={style}>
         <div ref={this.ref} />
       </div>
-    )
+    );
   }
 }
